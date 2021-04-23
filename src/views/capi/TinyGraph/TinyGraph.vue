f91e98c634c7c7eda9b8230784284a5a4195e662
jflorimo <flojeanc@yahoo.fr>
2021-04-23 20:26:00 +0200

    Add graph color

--- src/views/capi/TinyGraph/TinyGraph.vue
+++ src/views/capi/TinyGraph/TinyGraph.vue
@@ -3,7 +3,7 @@
 <svg width="135" height="50" viewBox="0 0 135 50" 
   xmlns="http://www.w3.org/2000/svg"
   xmlns:xlink="http://www.w3.org/1999/xlink">
-  <polyline v-bind:points="pointList" fill="none" stroke="#ed5565" stroke-width="1.25" preserveAspectRatio="none"/>
+  <polyline v-bind:points="pointList" fill="none" v-bind:stroke="stroke_color" stroke-width="1.25" preserveAspectRatio="none"/>
 </svg>
 </div>
 </template>
@@ -20,6 +20,7 @@ export default {
   data () {
     return {
       pointList: "",
+      stroke_color: "",
     }
   },
   computed: {
@@ -28,11 +29,16 @@ export default {
   methods: {
     setPoints() {
       let price_list = this.stock_last_7_days_close[this.marketId]["price_list"]
+      let len = price_list.length;
+
+      if (len === 0)
+        return
+
       let min_close = this.stock_last_7_days_close[this.marketId]["min"]
       let max_close = this.stock_last_7_days_close[this.marketId]["max"]
       let width = 135
       let height = 50
-      let len = price_list.length;
+
 
       var points = ""
       for (var i = 0; i < len; i++) {
@@ -41,6 +47,13 @@ export default {
         points += x+", "+y+" "
       }
       this.pointList = points
+
+      if (price_list[len-1] > price_list[0])
+        this.stroke_color = "#2eb85c"
+      else if (price_list[len-1] < price_list[0])
+        this.stroke_color = "#e55353"
+      else
+        this.stroke_color = "#4f5d73"
     }
     
   },
